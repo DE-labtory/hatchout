1435370e37edf19a86d447679a7233e1c8b49b94
minsoo <als950901@gmail.com>
2019-05-18 16:22:15 +0900

    feat: implement ghost contract [ISSUE-17]

---
+++ server/contracts/AccessControl.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.0;
+
+contract AccessControl {
+    address public ceoAddress;
+
+    modifier onlyCEO() {
+        require(msg.sender == ceoAddress);
+        _;
+    }
+
+    function setCEO(address _newCEO) external onlyCEO {
+        require(_newCEO != address(0));
+
+        ceoAddress = _newCEO;
+    }
+}
