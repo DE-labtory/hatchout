f04d80c9fa33815176b9159165ec0ae69181b020
hihiboss <junhui820@gmail.com>
2019-05-19 21:56:47 +0900

    feat: implement transaction service and module

---
+++ server/src/app/transaction/transaction.service.impl.spec.ts
@@ -0,0 +1,146 @@
+import {TransactionRepository} from '../../port/persistence/repository/transaction.repository.impl';
+import {mock, instance, when, anything, anyNumber} from 'ts-mockito';
+import {TransactionService} from './transaction.service.impl';
+import {Test, TestingModule} from '@nestjs/testing';
+import {Transaction} from '../../domain/transaction/transaction.entity';
+import {CreateTransactionRequest} from './request/create-transaction.request';
+
+describe('TransactionService', async () => {
+    const blockHash = '0x0000000000000000000000000000000000000000000000000000000012345678';
+    const blockNumber = 0;
+    const txHash = '0x0000000000000000000000000000000000000000000000000000000009101112';
+    const txIndex = 1;
+    const from = '0x1234000000000000000000000000000000000000';
+    const to = '0x5678000000000000000000000000000000000000';
+    const contractAddress = '0x0910000000000000000000000000000000000000';
+    const status = '0x0';
+
+    const mockRepository: TransactionRepository = mock(TransactionRepository);
+    const transaction: Transaction = new Transaction(
+        blockHash,
+        blockNumber,
+        txHash,
+        txIndex,
+        from,
+        to,
+        contractAddress,
+        status,
+    );
+    let service: TransactionService;
+
+    describe('dependency resolve', () => {
+        it('should be defined', async () => {
+            // given
+            const module: TestingModule = await Test.createTestingModule({
+                providers: [
+                    TransactionService,
+                    {
+                        provide: 'ITransactionRepository',
+                        useValue: instance(mockRepository),
+                    },
+                ],
+            }).compile();
+
+            // when
+            service = module.get<TransactionService>(TransactionService);
+
+            // then
+            expect(service).toBeDefined();
+        });
+    });
+
+    describe('#create()', () => {
+        let createTransactionRequest: CreateTransactionRequest;
+
+        it('should return created transaction', async () => {
+            when(mockRepository.findOne(txHash)).thenReturn(undefined);
+            when(mockRepository.save(anything())).thenReturn(new Promise((resolve) => {
+                resolve(transaction);
+            }));
+            service = new TransactionService(instance(mockRepository));
+            createTransactionRequest = new CreateTransactionRequest(
+                blockHash,
+                blockNumber,
+                txHash,
+                txIndex,
+                from,
+                to,
+                contractAddress,
+                status,
+            );
+
+            expect(await service.create(createTransactionRequest)).toBe(transaction);
+        });
+
+        it('should throw "txHash should defined"', async () => {
+            service = new TransactionService(instance(mockRepository));
+            createTransactionRequest = new CreateTransactionRequest();
+
+            await expect(service.create(createTransactionRequest))
+                .rejects
+                .toThrowError('txHash should be defined');
+        });
+
+        it('should throw "txHash is already registered"', async () => {
+            when(mockRepository.findOne(txHash)).thenReturn(new Promise((resolve) => {
+                resolve(transaction);
+            }));
+            service = new TransactionService(instance(mockRepository));
+            createTransactionRequest = new CreateTransactionRequest(
+                blockHash,
+                blockNumber,
+                txHash,
+                txIndex,
+                from,
+                to,
+                contractAddress,
+                status,
+            );
+
+            // then
+            await expect(service.create(createTransactionRequest))
+                .rejects
+                .toThrowError('txHash is already registered');
+        });
+    });
+
+    describe('#get()', () => {
+        it('should return transaction', async () => {
+            when(mockRepository.findOne(txHash)).thenReturn(new Promise((resolve) => {
+                resolve(transaction);
+            }));
+            service = new TransactionService(instance(mockRepository));
+
+            expect(await service.get(txHash)).toBe(transaction);
+        });
+
+        it('should return undefined', async () => {
+            const unsavedTxHash = '0x0000000000000000000000000000000000000000000000000000000013141516';
+            when(mockRepository.findOne(unsavedTxHash)).thenReturn(undefined);
+            service = new TransactionService(instance(mockRepository));
+
+            expect(await service.get(unsavedTxHash)).toBe(undefined);
+        });
+    });
+
+    describe('#getAll()', () => {
+        it('should return transaction list', async () => {
+            const txList: Transaction[] = [transaction];
+            const page = 1;
+            when(mockRepository.find(anything())).thenReturn(new Promise((resolve) => {
+                resolve(txList);
+            }));
+            service = new TransactionService(instance(mockRepository));
+
+            expect(await service.getAll(page)).toBe(txList);
+        });
+
+        it('should return undefined', async () => {
+            const page = 1;
+            when(mockRepository.find(anything())).thenReturn(undefined);
+            service = new TransactionService(instance(mockRepository));
+
+            expect(await service.getAll(page)).toBe(undefined);
+        });
+    });
+});
---
+++ server/src/app/transaction/transaction.service.impl.ts
@@ -0,0 +1,46 @@
+import {ITransactionService} from './transaction.service';
+import {Inject, Injectable} from '@nestjs/common';
+import {Transaction} from '../../domain/transaction/transaction.entity';
+import {ITransactionRepository} from '../../port/persistence/repository/transaction.repository';
+import {CreateTransactionRequest} from './request/create-transaction.request';
+
+@Injectable()
+export class TransactionService implements ITransactionService {
+    constructor(@Inject('ITransactionRepository') private TransactionRepository: ITransactionRepository) {}
+
+    async create(newTransactionRequest: CreateTransactionRequest): Promise<Transaction> {
+        const newTxHash = newTransactionRequest.txHash;
+
+        if (newTxHash === undefined) {
+            throw new Error('txHash should be defined');
+        }
+
+        const txRetrieved = await this.TransactionRepository.findOne(newTxHash);
+        if (txRetrieved !== undefined) {
+            throw new Error('txHash is already registered');
+        }
+
+        const newTx = new Transaction(
+            newTransactionRequest.blockHash,
+            newTransactionRequest.blockNumber,
+            newTransactionRequest.txHash,
+            newTransactionRequest.txIndex,
+            newTransactionRequest.from,
+            newTransactionRequest.to,
+            newTransactionRequest.contractAddress,
+            newTransactionRequest.status,
+        );
+        return await this.TransactionRepository.save(newTx);
+    }
+
+    async get(txHash: string): Promise<Transaction> {
+        return await this.TransactionRepository.findOne(txHash);
+    }
+
+    async getAll(page: number): Promise<Transaction[]> {
+        return await this.TransactionRepository.find({
+            take: 25,
+            skip: 25 * (page - 1),
+        });
+    }
+}
