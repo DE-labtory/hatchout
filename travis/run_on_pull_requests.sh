7c6ff16b1c92e61a9292f0e76ea0813ff40ff363
jun <leebduk@gmail.com>
2019-06-05 23:50:19 +0900

    chore: fix travis.yaml and scripts to support contracts test cases

--- travis/run_on_pull_requests.sh
+++ travis/run_on_pull_requests.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-# lint check
-npm run lint
+#### server ####
 
+# lint check
+npm run lint --prefix ./server/
 if [[ $? -ne 0 ]]; then
 echo "lint check error" >&2
 exit 1
 fi
 
 # run npm test
-npm run test
+npm run test --prefix ./server/
+
+if [[ $? -ne 0 ]]; then
+echo "npm test failed" >&2
+exit 1
+fi
+
+#### ethereum contract ####
+# run npm test
+npm run test --prefix ./contracts/ethereum/
 
 if [[ $? -ne 0 ]]; then
 echo "npm test failed" >&2
